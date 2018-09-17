f1db9a6cbef83dcaf887bb52e29b425b80e5c0be
Konrad Bartczak <konradbartczak@onet.pl>
2018-09-17 22:14:05 +0200

    argument matcher

--- src/test/java/konrad/junit/buisness/ListMockTest.java
+++ src/test/java/konrad/junit/buisness/ListMockTest.java
@@ -1,4 +1,40 @@
 package konrad.junit.buisness;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ListMockTest {
+    private List mock = mock(List.class);
+
+    @Test
+    public void size_basic() {
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void returnDifferentValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters() {
+        when(mock.get(0)).thenReturn("test");
+        assertEquals("test", mock.get(0));
+    }
+
+    @Test
+    public void returnWithGenericParameters() {
+        when(mock.get(anyInt())).thenReturn("test");//anyInt() - argument matcher
+        assertEquals("test", mock.get(0));
+        assertEquals("test", mock.get(4));
+    }
 }
