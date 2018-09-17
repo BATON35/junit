54704fd7cf1b71e38d4760682a9a2c05d6cadcdb
Konrad Bartczak <konradbartczak@onet.pl>
2018-09-17 22:57:42 +0200

    verify

--- src/test/java/konrad/junit/buisness/ListMockTest.java
+++ src/test/java/konrad/junit/buisness/ListMockTest.java
@@ -6,11 +6,10 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ListMockTest {
-    private List mock = mock(List.class);
+    private List<String> mock = mock(List.class);
 
     @Test
     public void size_basic() {
@@ -36,5 +35,19 @@ public class ListMockTest {
         when(mock.get(anyInt())).thenReturn("test");//anyInt() - argument matcher
         assertEquals("test", mock.get(0));
         assertEquals("test", mock.get(4));
+    }
+
+    @Test
+    public void verificationBasics() {
+        //SUT
+        String value = mock.get(0);
+        String valueVer1 = mock.get(1);
+        //Verify
+        verify(mock).get(0);
+        // verify(mock).get(anyInt());
+        verify(mock, times(2)).get(anyInt());//check times of invocation
+        verify(mock, atLeast(1)).get(anyInt());
+        verify(mock, atMost(3)).get(anyInt());
+        verify(mock, never()).get(2);
     }
 }
